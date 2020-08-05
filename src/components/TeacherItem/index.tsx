89a7ac00f2214c02204c33c979b995f703efdf87
lucas.farias <lucas.farias@ceuma.br>
2020-08-05 12:00:09 -0300

    Implementando componente item

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import whastappIcon from '../../assets//icons/whatsapp.svg';
+
+import './style.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/22084451?s=460&u=9164bc3700fed84b4de12d4b940ef0ba583675e0&v=4" alt="teacher" />
+        <div>
+          <strong>Lucas Farias</strong>
+          <span>Matemática</span>
+        </div>
+      </header>
+      <p>
+        The hammering of my brown mile was so vigorous, he soon found his kids on a swing joining his cunt stretcher deep in my balloon knot.
+            <br />
+            My cake hole was so full of cheese-crusted cock and man fat, the penis pudding was leaking down my chin and onto.
+          </p>
+
+      <footer>
+        <p>Preço/Hora <strong>R$ 25,00</strong></p>
+        <button type="button">
+          <img src={whastappIcon} alt="whatsapp" />
+              Entrar em contato
+            </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
