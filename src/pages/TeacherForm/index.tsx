45027f3ab19a52a2ad46f26ff3303ebbef922e7b
lucas.farias <lucas.farias@ceuma.br>
2020-08-05 12:00:55 -0300

    Implementando estrutura inicial do form

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Ser professor é impactar centenas de vidas"/>
+    </div>
+  );
+}
+
+export default TeacherForm;
