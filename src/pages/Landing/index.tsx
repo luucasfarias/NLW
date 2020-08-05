5294a74ad8589c3ad1ea7f4fe0127af38d0ed2f5
lucas.farias <lucas.farias@ceuma.br>
2020-08-05 12:01:16 -0300

    Finalizando componente landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -21,14 +22,14 @@ function Landing() {
         <img src={landingImg} alt="landing" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="study"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="give class"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
